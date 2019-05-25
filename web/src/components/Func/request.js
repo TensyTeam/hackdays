644c1d0919326772a7303b029414e8fd05e95416
mike-petrov <mis5116@yandex.ru>
2019-05-25 17:57:05 +0200

    try to adapter

---
+++ web/src/components/Func/request.js
@@ -0,0 +1,16 @@
+export function getSensors(activeTab) {
+    return "https://api.disruptive-technologies.com/v2/projects/bjfb7m3httut79sc9hsg/devices/bgutlokc00010i5633u0";
+}
+
+export function serverResponse(urlRequest) {
+    let request = new XMLHttpRequest();
+    request.open('GET', urlRequest, false);
+    request.send();
+    if (request.status !== 200) {
+        console.log(request.status + ': ' + request.statusText);
+    } else {
+        let response = request.responseText;
+        response = JSON.parse(response);
+        return response;
+    }
+}
