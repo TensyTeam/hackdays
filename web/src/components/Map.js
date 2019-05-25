46e19fa8e625a3c46f1b25c92a2b9f4ed92e5338
mike-petrov <mis5116@yandex.ru>
2019-05-25 15:50:05 +0200

    update old func

--- web/src/components/Map.js
+++ web/src/components/Map.js
@@ -3,15 +3,33 @@ import { Link } from "react-router-dom"
 
 
 export default class Map extends React.Component {
+	constructor (props) {
+		super(props);
+		this.state = {
+			path: null
+		};
+		this.onChange = this.onChange.bind(this);
+	}
+
+	componentWillMount() {
+		this.setState({ path: document.location.pathname });
+	}
+
+	onChange(_path) {
+		this.setState({ path: _path });
+	}
+
 	render() {
 		return (
 			<div className="map">
 				<div>
-					<Link to="/map/temperature">Temperature</Link>
-					<Link to="/map/density">Density</Link>
+					<Link to="/map/temperature" onClick={()=>{this.onChange('/map/temperature')}}>Temperature</Link>
+					<Link to="/map/density" onClick={()=>{this.onChange('/map/density')}}>Density</Link>
 				</div>
-				{document.location.pathname === '/map/temperature' ?
-					<img src="../../img/map1.png"/> :
+				{this.state.path === '/map/temperature' &&
+					<img src="../../img/map1.png"/>
+				}
+				{this.state.path === '/map/density' &&
 					<img src="../../img/map2.png"/>
 				}
 			</div>
--- web/src/components/Tabbar.js
+++ web/src/components/Tabbar.js
@@ -6,9 +6,9 @@ export default class Tabbar extends React.Component {
 	render() {
 		return (
 			<div className="tabbar">
-				<Link to="/timeline"><i class="fas fa-calendar-alt"></i></Link>
+				<Link to="/timeline"><i className="fas fa-calendar-alt"></i></Link>
 				<Link to="/services"><img src="./img/logo.png"/></Link>
-				<Link to="/map/temperature"><i class="fas fa-map"></i></Link>
+				<Link to="/map/temperature"><i className="fas fa-map"></i></Link>
 			</div>
 		)
 	}
