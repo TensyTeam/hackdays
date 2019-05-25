fb60af9dde782303900f7bb7e30548b5593ad142
mike-petrov <mis5116@yandex.ru>
2019-05-25 15:01:33 +0200

    Updates

---
+++ precache-manifest.637a4db44690a2d2071d174f496bc9ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "60443b75f30b9a23455b",
+    "url": "/hackdays/static/css/main.959aad08.chunk.css"
+  },
+  {
+    "revision": "60443b75f30b9a23455b",
+    "url": "/hackdays/static/js/main.60443b75.chunk.js"
+  },
+  {
+    "revision": "7742c67cb28e9a780dea",
+    "url": "/hackdays/static/js/runtime~main.7742c67c.js"
+  },
+  {
+    "revision": "12a9643e057593619edf",
+    "url": "/hackdays/static/js/2.12a9643e.chunk.js"
+  },
+  {
+    "revision": "6f4c2e67fbe83c118530f16cf7b9c7ce",
+    "url": "/hackdays/index.html"
+  }
+];
